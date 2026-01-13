c033f7e2df65990dc17a0e856c28bb8ae14e8645
Ale-Deto04 <ale.detoffoli@gmail.com>
2026-01-13 12:20:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Build docker containers
 docker build -f Dockerfiles/Dockerfile.ryu -t kathara/ryu .
 docker build -f Dockerfiles/Dockerfile.host -t kathara/host .
 docker build -f Dockerfiles/Dockerfile.server -t kathara/server .
+
+# Make init script executable
+chmod +x Kathara/shared/scripts/init.sh
